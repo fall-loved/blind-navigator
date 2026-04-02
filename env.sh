78e1fdf448e8c477d7e7b3b7c01a8557d3929579
fall-loved <falloved24@gmail.com>
2026-04-02 17:03:08 +0300

    Delete env.sh

--- env.sh
+++
@@ -1,5 +0,0 @@
-export JAVA_HOME="/usr/lib/jvm/java-21-openjdk"
-export ANDROID_HOME=$HOME/Android/Sdk
-export PATH=$PATH:$ANDROID_HOME/emulator
-export PATH=$PATH:$ANDROID_HOME/platform-tools
-export PATH=$PATH:$ANDROID_HOME/cmdline-tools/latest/bin
